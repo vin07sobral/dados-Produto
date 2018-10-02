9cf45409e01e1f7a538a301a1dfb4ebd3dba8182
Marcus Vinicius Sobral <mvinicius.sobral@gmail.com>
2018-10-02 12:40:18 -0300

    Add files via upload

---
+++ dados-produto/dados-produto/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace dados_produto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int qte;
+            produto p;
+            p = new produto();
+
+            Console.WriteLine("Digite o nome do produto:");
+            Console.Write("Nome: ");
+            p.nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade em Estoque: ");
+            p.quantidadeEmEstoque = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do Produto: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite a quantidade de produtos que entraram no estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.realizarEntrada(qte);
+
+            Console.WriteLine();
+            Console.Write("Dados atualizados: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite a quantidade de produtos que sairam no estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.realizarSaida(qte);
+
+            Console.WriteLine();
+            Console.Write("Dados atualizados: " + p);
+
+
+
+
+            Console.ReadLine();
+          
+        }
+    }
+}
---
+++ dados-produto/dados-produto/produto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace dados_produto
+{
+    class produto
+    {
+        public string nome;
+        public double preco;
+        public int quantidadeEmEstoque;
+
+        public double valorTotalEmEstoque()
+        {
+            return preco * quantidadeEmEstoque;
+        }
+
+        public void realizarEntrada(int quantidade)
+        {
+            quantidadeEmEstoque = quantidadeEmEstoque + quantidade;
+        }
+
+        public void realizarSaida(int quantidade)
+        {
+            quantidadeEmEstoque = quantidadeEmEstoque - quantidade;
+        }
+
+        public override string ToString()
+        {
+            return nome
+                + ", R$"
+                + preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + quantidadeEmEstoque
+                + " unidades, Total: R$: "
+                + valorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
